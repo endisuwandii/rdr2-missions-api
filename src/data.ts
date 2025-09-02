e4d180fd7925a6b708361d7e51a8727b8b91982b
endisuwandi <endisuwandi64@gmail.com>
2025-09-02 14:21:00 +0700

    feat menambhakan file baru yaitu data.ts

---
+++ src/data.ts
@@ -0,0 +1,23 @@
+export const missions = [
+  {
+    id: 1,
+    chapter: 1,
+    title: "Outlaws from the West",
+    description:
+      "Arthur dan geng mencari perlindungan di pegunungan bersalju setelah perampokan yang gagal.",
+  },
+  {
+    id: 2,
+    chapter: 1,
+    title: "Enter, Pursued by a Memory",
+    description:
+      "Arthur dan Javier mencari John Marston yang hilang di tengah badai salju.",
+  },
+  {
+    id: 3,
+    chapter: 2,
+    title: "Polite Society, Valentine Style",
+    description:
+      "Arthur menjual beberapa barang curian dan membuat masalah di kota Valentine.",
+  },
+];
