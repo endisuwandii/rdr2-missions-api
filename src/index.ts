f76b9919f26dc81519840ac25373ce0168ce9af8
endisuwandi <endisuwandi64@gmail.com>
2025-09-02 14:22:23 +0700

    mengubah kode yang eror di alibatkan syntax kurang benar

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { missions } from "./data";
+const app = new Hono();
 
-const app = new Hono()
+app.get("/", (c) => {
+  return c.text("Hello User!");
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/missions", (c) => {
+  return c.json(missions);
+});
+app.get("/missions/:id", (c) => {
+  const id = Number(c.req.param("id"));
+  const mission = missions.find((m) => m.id === id);
+  if (mission) {
+    return c.json(mission);
+  } else {
+    return c.json({ message: "Misi tidak ditemukan" }, 404);
+  }
+});
 
-export default app
+export default app;
