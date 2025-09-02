5348a6c5484270c3cc1700a6cb35bf30adab9cdd
endisuwandi <endisuwandi64@gmail.com>
2025-09-02 14:33:46 +0700

    memperbaharui isi dari file data.ts menjadi 30 daftar misi

--- src/data.ts
+++ src/data.ts
@@ -20,4 +20,182 @@ export const missions = [
     description:
       "Arthur menjual beberapa barang curian dan membuat masalah di kota Valentine.",
   },
+  {
+    id: 4,
+    chapter: 2,
+    title: "Americans at Rest",
+    description:
+      "Geng beristirahat di Horseshoe Overlook dan Arthur menjelajahi kota Valentine.",
+  },
+  {
+    id: 5,
+    chapter: 2,
+    title: "Paying a Social Call",
+    description:
+      "Arthur, Javier, dan Charles mengunjungi kediaman Downes untuk menagih hutang.",
+  },
+  {
+    id: 6,
+    chapter: 2,
+    title: "A Quiet Time",
+    description:
+      "Lenny dan Arthur pergi ke salon di Valentine untuk bersenang-senang, tetapi mereka mabuk berat.",
+  },
+  {
+    id: 7,
+    chapter: 3,
+    title: "The Fine Joys of Civilization",
+    description:
+      "Geng pindah ke Clemens Point, dekat Rhodes. Arthur dan Hosea menyamar untuk menipu keluarga Gray.",
+  },
+  {
+    id: 8,
+    chapter: 3,
+    title: "Blood Feuds, Ancient and Modern",
+    description:
+      "Arthur dan sekelompok anggota geng membantu keluarga Gray, tetapi mereka harus waspada terhadap Braithwaite.",
+  },
+  {
+    id: 9,
+    chapter: 3,
+    title: "The Course of True Love",
+    description:
+      "Arthur membantu Trelawny untuk membebaskan dua temannya yang diculik.",
+  },
+  {
+    id: 10,
+    chapter: 3,
+    title: "Sodomy, Back in the West",
+    description:
+      "Arthur membantu Reverend Swanson kembali ke kamp setelah dia mabuk.",
+  },
+  {
+    id: 11,
+    chapter: 3,
+    title: "Blessed Are the Peacemakers",
+    description:
+      "Arthur membantu Dutch dan Hosea bertemu dengan musuh mereka untuk bernegosiasi.",
+  },
+  {
+    id: 12,
+    chapter: 3,
+    title: "An Honest Mistake",
+    description:
+      "Misi yang melibatkan kereta api yang salah dan banyak tembakan.",
+  },
+  {
+    id: 13,
+    chapter: 4,
+    title: "The Gilded Cage",
+    description:
+      "Geng pindah ke Saint Denis. Arthur membantu Hosea menghadiri pesta mewah.",
+  },
+  {
+    id: 14,
+    chapter: 4,
+    title: "Revenge Is a Dish Best Eaten",
+    description:
+      "Arthur dan Sadie mencari orang-orang yang membunuh suami Sadie.",
+  },
+  {
+    id: 15,
+    chapter: 4,
+    title: "Urban Pleasures",
+    description:
+      "Arthur, Hosea, dan Trelawny menjalankan misi untuk mencuri barang berharga.",
+  },
+  {
+    id: 16,
+    chapter: 4,
+    title: "The Joys of Civilization",
+    description:
+      "Arthur bertemu dengan Miss Downes dan membantunya di Saint Denis.",
+  },
+  {
+    id: 17,
+    chapter: 4,
+    title: "A Quick Fix",
+    description: "Dutch, Arthur, dan Bill merampok bank di Saint Denis.",
+  },
+  {
+    id: 18,
+    chapter: 5,
+    title: "Welcome to Guarma",
+    description:
+      "Setelah perampokan bank gagal, geng terdampar di pulau terpencil.",
+  },
+  {
+    id: 19,
+    chapter: 5,
+    title: "A Strange and New Land",
+    description: "Arthur dan Dutch mencoba melarikan diri dari Guarma.",
+  },
+  {
+    id: 20,
+    chapter: 5,
+    title: "That's Murfee's Brood",
+    description:
+      "Geng kembali ke Amerika dan mencari tahu apa yang terjadi pada camp mereka.",
+  },
+  {
+    id: 21,
+    chapter: 6,
+    title: "The King's Son",
+    description:
+      "Dutch, Arthur, dan Sadie kembali ke Saint Denis untuk misi penyerbuan.",
+  },
+  {
+    id: 22,
+    chapter: 6,
+    title: "The Fine Art of Conversation",
+    description: "Arthur mencari tempat aman bagi Abigail dan John.",
+  },
+  {
+    id: 23,
+    chapter: 6,
+    title: "A Fork in the Road",
+    description: "Arthur harus membuat keputusan yang menentukan takdirnya.",
+  },
+  {
+    id: 24,
+    chapter: 6,
+    title: "Just a Social Call",
+    description: "Arthur dan Charles mengunjungi kediaman O'Driscoll.",
+  },
+  {
+    id: 25,
+    chapter: 6,
+    title: "The Sins of the Father",
+    description: "Arthur mencoba membantu orang-orang di Saint Denis.",
+  },
+  {
+    id: 26,
+    chapter: 6,
+    title: "Our Best Selves",
+    description: "Arthur dan Dutch melakukan misi terakhir untuk geng.",
+  },
+  {
+    id: 27,
+    chapter: 6,
+    title: "Red Dead Redemption",
+    description: "Misi terakhir untuk Arthur Morgan.",
+  },
+  {
+    id: 28,
+    chapter: 6,
+    title: "My Last Boy",
+    description: "Arthur dan Dutch menyelesaikan misi yang rumit.",
+  },
+  {
+    id: 29,
+    chapter: 6,
+    title: "The Sins of Our Fathers",
+    description: "Arthur membantu John untuk melarikan diri dari geng.",
+  },
+  {
+    id: 30,
+    chapter: 6,
+    title: "Final Mission",
+    description: "Tugas terakhir Arthur Morgan untuk geng dan keluarganya.",
+  },
 ];
