35d24604cb9dde6cf1a43015f38503a38566288d
endisuwandi <endisuwandi64@gmail.com>
2025-09-23 20:28:41 +0700

    remove file

--- prisma/migrations/20250920153001_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."Mission" (
-    "id" SERIAL NOT NULL,
-    "chapter" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-
-    CONSTRAINT "Mission_pkey" PRIMARY KEY ("id")
-);
