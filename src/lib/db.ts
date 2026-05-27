b5dd8268f80294a35869928b6afd28c7115c3614
endisuwandi <endisuwandi64@gmail.com>
2026-05-27 20:04:37 +0700

    chore: clean up import statements in db.ts for consistency

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,21 +1,15 @@
-
-import { Pool } from "pg"; 
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-
-import { PrismaClient } from "../generated/prisma"; 
-
+import { PrismaClient } from "@prisma/client"; // 👈 Ini jalur import yang bener
 
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
   throw new Error("Tolong masukkan DATABASE_URL di environment variables!");
 }
 
-
 const pool = new Pool({ connectionString });
 
-
 const adapter = new PrismaPg(pool);
-
 
 const db = new PrismaClient({ adapter });
 
